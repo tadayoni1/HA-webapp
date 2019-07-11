098b103680cb0fa9a1a166ff45e5a8a3bdcef30e
Pedram <tadayoni@gmail.com>
2019-07-10 23:07:26 -0700

    add network cfn and bash scripts

---
+++ create_network.sh
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name myNetworkStack --template-body file://network.yml --parameters file://network-parameters.json --region us-west-2
---
+++ update_network.sh
@@ -0,0 +1,1 @@
+aws cloudformation update-stack --stack-name myNetworkStack --template-body file://network.yml --parameters file://network-parameters.json --region us-west-2
